17acfdcba8213170ce9f9d75251828b50e20120a
sharn <btssharndeep762@gmail.com>
2025-10-06 22:37:13 +0530

    this is correct code

--- this.cpp
+++ this.cpp
@@ -1,26 +1,36 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
-class test{
-    public:
 
-    int number(string name){
-        int sum=0;
-        for(int i=0;i<name.length();i++){
-            int j;
-            for( j=0;j<name.length();j++){
-                if(name[i]==name[j]){
+class test {
+public:
+    void number(string name) {
+        int maxFreq = 0;
+        char maxChar = '\0';
+
+        for (int i = 0; i < name.length(); i++) {
+            int sum = 0;
+            for (int j = 0; j < name.length(); j++) {
+                if (name[i] == name[j]) {
                     sum++;
                 }
             }
-            int max= sum;
-            cout<<"max is :"<<name[i]<<" "<<max<<endl;
-            sum= 0;
+
+            // agar current char ki frequency max se zyada hai
+            if (sum > maxFreq) {
+                maxFreq = sum;
+                maxChar = name[i];
+            }
         }
+
+        cout << "Most frequent character: " << maxChar << endl;
+        cout << "Frequency: " << maxFreq << endl;
     }
 };
-int main(){
+
+int main() {
     test obj;
-    string name= "sharndeepsinghhh";
+    string name = "sharndeepsinghhh";
     obj.number(name);
     return 0;
 }
